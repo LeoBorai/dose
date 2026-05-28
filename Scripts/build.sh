95c94a5ce3230b596e57c0c035e55a0501bf937b
Leo Borai <estebanborai@gmail.com>
2026-05-28 22:14:07 +0200

    feat: do not chmod

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,5 +4,4 @@ swift build -c release
 mkdir -p dist/dose.app/Contents/MacOS
 mkdir -p dist/dose.app/Contents/Resources
 mv .build/release/dose dist/dose.app/Contents/MacOS/dose
-chmod +x dist/dose.app/Contents/MacOS/dose
 cp ./Info.plist dist/dose.app/Contents/Info.plist
